d64307660c56d94f0456dfc9309cb285b5864f6e
RK <rkfrmda3@gmail.com>
2026-06-27 16:19:35 -0500

    Refactor fetchNui and update import statement
    Refactor fetchNui function to improve readability and update import path.

--- web/src/utils/fetchNui.ts
+++ web/src/utils/fetchNui.ts
@@ -1,39 +1,30 @@
-import { isEnvBrowser } from "./misc";
+import { isEnvBrowser } from './env';
 
 /**
- * Simple wrapper around fetch API tailored for CEF/NUI use. This abstraction
- * can be extended to include AbortController if needed or if the response isn't
- * JSON. Tailor it to your needs.
+ * Simple wrapper around fetch API tailored for CEF/NUI use.
  *
  * @param eventName - The endpoint eventname to target
  * @param data - Data you wish to send in the NUI Callback
  * @param mockData - Mock data to be returned if in the browser
- *
- * @return returnData - A promise for the data sent back by the NuiCallbacks CB argument
  */
-
 export async function fetchNui<T = unknown>(
   eventName: string,
   data?: unknown,
   mockData?: T,
 ): Promise<T> {
   const options = {
-    method: "post",
+    method: 'post',
     headers: {
-      "Content-Type": "application/json; charset=UTF-8",
+      'Content-Type': 'application/json; charset=UTF-8',
     },
     body: JSON.stringify(data),
   };
 
-  if (isEnvBrowser() && mockData) return mockData;
+  if (isEnvBrowser()) return mockData as T;
 
-  const resourceName = (window as any).GetParentResourceName
-    ? (window as any).GetParentResourceName()
-    : "nui-frame-app";
+  const resourceName = window.GetParentResourceName?.() ?? 'nui-frame-app';
 
   const resp = await fetch(`https://${resourceName}/${eventName}`, options);
 
-  const respFormatted = await resp.json();
-
-  return respFormatted;
+  return resp.json() as Promise<T>;
 }
