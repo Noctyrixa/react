a15b54a40380b303cfc7b51e6be307133ec8939d
RK <rkfrmda3@gmail.com>
2026-06-27 16:17:50 -0500

    Add NUI actions and callbacks constants

---
+++ web/src/constants/events.ts
@@ -0,0 +1,9 @@
+export const NUI_ACTIONS = {
+  OPEN: 'open',
+  CLOSE: 'close',
+} as const;
+
+export const NUI_CALLBACKS = {
+  HIDE_FRAME: 'hideFrame',
+  GET_CLIENT_DATA: 'getClientData',
+} as const;
