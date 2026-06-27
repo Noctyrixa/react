29676209a3663477f75ae12f84c65274313a197f
RK <rkfrmda3@gmail.com>
2026-06-27 16:17:02 -0500

    Refactor React rendering with createRoot and StrictMode
    Updated rendering method to use createRoot and added StrictMode wrapper.

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { VisibilityProvider } from './providers/VisibilityProvider';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './components/App';
+import { NUI_ACTIONS } from './constants/events';
+import { setupEmulate } from './dev/emulate';
+import { VisibilityProvider } from './providers/VisibilityProvider';
+import { isEnvBrowser } from './utils/env';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <VisibilityProvider>
+setupEmulate();
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <VisibilityProvider
+      defaultVisible={isEnvBrowser()}
+      showActions={[NUI_ACTIONS.OPEN]}
+      hideActions={[NUI_ACTIONS.CLOSE]}
+    >
       <App />
     </VisibilityProvider>
-  </React.StrictMode>,
+  </StrictMode>,
 );
