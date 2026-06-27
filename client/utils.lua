b292a41f8a264b57b21b01e018380ebadd58e03c
RK <rkfrmda3@gmail.com>
2026-06-27 16:21:31 -0500

    Refactor debug functions and remove SendReactMessage

--- client/utils.lua
+++ client/utils.lua
@@ -1,30 +1,14 @@
---- A simple wrapper around SendNUIMessage that you can use to
---- dispatch actions to the React frame.
----
----@param action string The action you wish to target
----@param data any The data you wish to send along with this action
-function SendReactMessage(action, data)
-  SendNUIMessage({
-    action = action,
-    data = data
-  })
-end
+local resource = GetCurrentResourceName()
+local debugEnabled = GetConvarInt('ntx:debug', 0) == 1
 
-local currentResourceName = GetCurrentResourceName()
+---@param ... any
+function debug(...)
+  if not debugEnabled then return end
 
-local debugIsEnabled = GetConvarInt(('%s-debugMode'):format(currentResourceName), 0) == 1
-
---- A simple debug print function that is dependent on a convar
---- will output a nice prettfied message if debugMode is on
-function debugPrint(...)
-  if not debugIsEnabled then return end
-  local args <const> = { ... }
-
-  local appendStr = ''
-  for _, v in ipairs(args) do
-    appendStr = appendStr .. ' ' .. tostring(v)
+  local parts = {}
+  for i = 1, select('#', ...) do
+    parts[i] = tostring(select(i, ...))
   end
-  local msgTemplate = '^3[%s]^0%s'
-  local finalMsg = msgTemplate:format(currentResourceName, appendStr)
-  print(finalMsg)
+
+  print(('^3[ntx]^7 ^5[%s]^7 %s'):format(resource, table.concat(parts, ' ')))
 end
